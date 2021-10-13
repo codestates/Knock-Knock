e9ed12f6c0c583a795749c3701d8bedb674d9eef
Cho-Janghoon <dadasince2016@gmail.com>
2021-10-13 14:39:07 +0900

    modified dev

---
+++ client/src/pages/MyList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import axios from 'axios'
+import "./MyList.css"
+
+const MyList = ({writeInfo}) => {
+
+    
+
+    return (
+        <div>
+            {/* <span>{writeInfo.toilet}</span>
+            <span>{writeInfo.comment}</span> */}
+        </div>
+    )
+}
+
+export default MyList
